5b238435b12a30cbb9953fa256a1b8032bad1a05
by-yoyo <168732020+by-yoyo@users.noreply.github.com>
2026-04-05 16:19:09 +0800

    优化配置 修改工作流

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,18 @@
-import type { NextConfig } from 'next';
-import { getHeaders } from './next_config/headers';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	transpilePackages: ['three'],
-	output: 'standalone',
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'avatars.githubusercontent.com',
-				pathname: '/**',
-			},
-		],
-	},
-	reactCompiler: true,
-	async headers() {
-		if (process.env.NODE_ENV !== 'production') {
-			return [];
-		}
-		return getHeaders();
-	},
+  transpilePackages: ["three"],
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
